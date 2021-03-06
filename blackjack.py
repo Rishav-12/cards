809046e2fd037295d193ab80d495a9402c957037
Rishav <rishavmitra10@gmail.com>
2021-03-07 02:15:34 +0530

    Cleaner code

--- blackjack.py
+++ blackjack.py
@@ -24,28 +24,42 @@ for _ in range(2):
 	player_hand.append(deck.pop())
 	dealer_hand.append(deck.pop())
 
+# this function displays the cards in a given hand
 def show_hand(who, hand):
 	print("\n" + who + "\n")
 	for card in hand:
 		print(card.show_card_info())
 
 # this function checks for a win due to a blackjack or a loss by busting
-def check(who, hand):
+def check_direct_result(who, hand):
 	hand_sum = sum([card.bjvalue for card in hand])
+
 	if hand_sum > 21:
 		print(f"\n{who} busts and loses")
 		sys.exit()
-	else: # adjusting for an ace here
+
+	else:
 		for card in hand:
-			if card.rank == "Ace":
+			if card.rank == "Ace": # adjusting for an ace here
 				hand_sum += 10
 				if hand_sum == 21 and len(hand) == 2:
 					print(f"\n{who} wins by getting a blackjack")
 					sys.exit()
 				elif hand_sum > 21:
 					hand_sum -= 10
+
 	return hand_sum
 
+def check_both():
+	player_sum = check_direct_result("Player", player_hand)
+	dealer_sum = check_direct_result("Dealer", dealer_hand)
+	return player_sum, dealer_sum
+
+def show_result():
+	show_hand("Player", player_hand)
+	show_hand("Dealer", dealer_hand)
+	print("Player has a hand value of", player_sum)
+	print("Dealer has a hand value of", dealer_sum)
 
 print("\nAfter dealing")
 show_hand("Player", player_hand)
@@ -53,35 +67,26 @@ show_hand("Player", player_hand)
 print("\nDealer")
 print(f"Has a hidden card and {dealer_hand[1].show_card_info()}")
 
-player_sum = check("Player", player_hand)
-dealer_sum = check("Dealer", dealer_hand)
+player_sum, dealer_sum = check_both()
 print("Player has a hand value of", player_sum)
 
 while True:
 	opt = input("\nWould you like to hit or stand ? ") # asking player if they want to hit or stand
-	if opt not in ['hit', 'stand']:
+	if opt not in ['hit', 'h', 'stand', 's']:
 		continue
-	if opt == 'hit':
+	if opt == 'hit' or opt == 'h':
 		player_hand.append(deck.pop()) # Add the card
 		show_hand("Player", player_hand) # show the complete hand
-		# check for win after every card is added
-		player_sum = check("Player", player_hand)
-		dealer_sum = check("Dealer", dealer_hand)
+		player_sum, dealer_sum = check_both() # check for win after every card is added
 		print("Player has a hand value of", player_sum)
-	if opt == 'stand':
+	if opt == 'stand' or opt == 's':
 		break
 
 while dealer_sum < 17: # the dealer takes a card until his hand exceeds 17
 	dealer_hand.append(deck.pop())
-	player_sum = check("Player", player_hand)
-	dealer_sum = check("Dealer", dealer_hand)
+	player_sum, dealer_sum = check_both()
 
-
-show_hand("Player", player_hand)
-show_hand("Dealer", dealer_hand)
-
-print("Player has a hand value of", player_sum)
-print("Dealer has a hand value of", dealer_sum)
+show_result()
 
 # Compare hands and check winner
 if player_sum > dealer_sum:
